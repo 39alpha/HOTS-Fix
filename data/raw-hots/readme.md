ca1b31b7cc7495f5314e57628d97a0a13e6402cc
Cole Mathis <cole.mathis.ool@gmail.com>
2022-08-12 15:04:39 -0700

    Adding script to extract netcdf to combined csv

---
+++ data/raw-hots/readme.md
@@ -0,0 +1,43 @@
+This data is from Hawaiian Ocean timeseries datasets. 
+
+It was collected on February 26th 2022 using the HOT-DOGS web tool (https://hahana.soest.hawaii.edu/hot/hot-dogs/bextraction.html)
+
+The Data comes from the `Bottle` Selection tool. 
+
+The following query was selected:
+
+Stations: 1,2,6,8,50,52 (all were performed in sequence as different queries)
+
+Dates (default range): Oct, 0, 1, 1988 - Dec 3, 1, 2020
+
+Fields:
+- Potential Temperature 
+- Potential Density 
+- CTD Temperature
+- CTD Salinity 	
+- CTD Oxygen 
+- Bottle Salinity
+- Bottle Dissolved Oxygen 
+- Dissolved Inorganic Carbon 
+- pH
+- Alkalinity 
+- Phosphate 
+- Nitrate + Nitrite
+- Nitrite 
+- Silicate 
+- Dissolved Organic Phosphorus
+- Dissolved Organic Nitrogen 
+- Dissolved Organic Carbon 
+- Total Dissolved Phosphorus
+- Total Dissolved Nitrogen 
+- Particulate Phosphorus 
+- Particulate Nitrogen
+- Particulate Silica 
+- Particulate Carbon 
+- Low-Level Phosphorus 
+- Low-Level Nitrogen 
+
+A warning was raised that Analytical Methods have changed over the time range. 
+
+
+Cole Mathis February 26th 2022. CSS0.
---
+++ hots-fix/extract-and-clean.py
@@ -0,0 +1,35 @@
+import netCDF4
+import pandas as pd
+import glob
+
+def get_dataframe_from_cdf(cdf_file):
+    """ Convert the data from a netCDF file to a DataFrame.
+        Ignore variables called ["dataDesc, "ros", "mtime"]
+        
+        Args: cdf_file - path to netcdf file
+        Returns: this_df - DataFrame containing the data from the netcdf as columns
+    """
+    # Read the data
+    data = netCDF4.Dataset(cdf_file)
+    # Initialize the dataframe
+    this_df = pd.DataFrame()
+    # Iterate over variables
+    for var in data.dimensions:
+        # If they're not in the exclude list
+        if var not in ["dataDesc", "ros", "mtime"]:
+            this_df[var] = data[var][:]
+    return this_df
+
+def convert_all_cdf_to_csv(path):
+    """ Convert all the .netCDF files in the path to a DataFrame.
+    """
+    all_netcdf_files = glob.glob(path + "*.nc")
+    first_df = get_dataframe_from_cdf(all_netcdf_files[0])
+    for net_cdf_file in all_netcdf_files[1:]:
+        this_df = get_dataframe_from_cdf(net_cdf_file)
+        first_df = pd.concat([first_df, this_df], ignore_index=False)
+    
+    first_df.to_csv(path + "raw_combined_data.csv")
+
+if __name__ == "__main__":
+    convert_all_cdf_to_csv("..\\data\\raw-hots\\")
