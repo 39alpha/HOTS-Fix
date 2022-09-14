4d3da94e567451f9ae62d3f8a1d71f1d9f977170
Cole Mathis <cole.mathis.ool@gmail.com>
2022-09-14 16:14:39 -0700

    Clean data written, next step train the model

--- hots-fix/extract-and-clean.py
+++ hots-fix/extract-and-clean.py
@@ -31,5 +31,22 @@ def convert_all_cdf_to_csv(path):
     
     first_df.to_csv(path + "combined_data.csv")
 
+
+def clean_hots():
+    """Clean the raw data from HOTS to make sure it's suitable for training the prediction method.
+    """
+    combined_data = pd.read_csv("..\\data\\raw-hots\\combined_data.csv", index_col = 0)
+    minimum_data = combined_data.query("press > 0 & temp > 0 & dic > 0 & ph > 0")
+    keeper_cols = ['crn','stn','cast','press',
+                   'theta','sigma','temp',
+                   'csal','coxy','dic','ph',
+                   'alk','phos','nit','sil',
+                   'doc']
+    keep_min_data = combined_data[keeper_cols]
+    keep_min_data = keep_min_data[(keep_min_data > 0).all(1)]
+
+    keep_min_data.to_csv("..\\data\parsed\\ideal-variables-2022.csv")
+
 if __name__ == "__main__":
-    convert_all_cdf_to_csv("..\\data\\raw-hots\\")
+    # convert_all_cdf_to_csv("..\\data\\raw-hots\\")
+    clean_hots()
