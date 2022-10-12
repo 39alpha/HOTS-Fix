76bfe46f902e63e9dfd15533ac7cfe9fffa99dfd
Cole Mathis <cole.mathis.ool@gmail.com>
2022-10-12 16:02:07 -0700

    rename to just use 'predict' as the main function

--- hots_fix/__init__.py
+++ hots_fix/__init__.py
@@ -1,4 +1,7 @@
-from .predict import load_all_models, predict_from_cast
+from .predictions import load_all_models, predict_from_cast
+
 all_models = load_all_models()
-def predict_hots(var, cast_data):
+
+def predict(var, cast_data):
+    """ DOC-STRING FOR PREDICTION """
     return predict_from_cast(var, cast_data, all_models)
